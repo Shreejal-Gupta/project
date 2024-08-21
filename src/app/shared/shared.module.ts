60c70399f61b89abedcf042eae9ac33bb348985c
omitbajracharya <omitbajracharya@gmail.com>
2024-08-21 17:09:14 +0545

    landing page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from './prime-ng.module';
-
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    
+    DiagramModule,
     PrimeNgModule
   ],
-  exports: [PrimeNgModule]
+  exports: [PrimeNgModule,DiagramModule]
 })
 export class SharedModule { }
